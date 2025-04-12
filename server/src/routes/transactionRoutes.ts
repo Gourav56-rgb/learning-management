9b09cd0f1ec352dbb0e78e2b2e3c2fc807736d57
GOURAV KUMAR BEHERA <sourav7712389360@gmail.com>
2025-04-12 14:22:28 +0530

    initial

---
+++ server/src/routes/transactionRoutes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { createStripePaymentIntent, createTransaction, listTransactions } from "../controllers/transactionController";
+
+const router = express.Router();
+
+router.get("/", listTransactions)
+router.post("/stripe/payment-intent", createStripePaymentIntent);
+router.post("/", createTransaction);
+
+export default router;
