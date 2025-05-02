410702c6f4313f89a75c2d72004cd320baf165f3
GOURAV KUMAR BEHERA <85735841+Gourav56-rgb@users.noreply.github.com>
2025-05-02 15:10:43 +0530

    Update NonDashboardNavbar.tsx

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -8,7 +8,7 @@ import React from "react";
 
 const NonDashboardNavbar = () => {
   const { user } = useUser();
-  const userRole = user?.publicMetadata?.userType as "teacher" | "student";
+  const userRole = user?.publicMetadata?.userType as "student" | "teacher";
 
   return (
     <nav className="nondashboard-navbar">
