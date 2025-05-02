84166ac8b3bd931f0d269f222ab7fe30f2af2177
GOURAV KUMAR BEHERA <85735841+Gourav56-rgb@users.noreply.github.com>
2025-05-02 14:59:53 +0530

    Update middleware.ts

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -7,8 +7,8 @@ const isTeacherRoute = createRouteMatcher(["/teacher/(.*)"]);
 export default clerkMiddleware(async (auth, req) => {
   const { sessionClaims } = await auth();
   const userRole =
-    (sessionClaims?.metadata as { userType: "student" | "teacher" })
-      ?.userType || "student";
+    (sessionClaims?.metadata as { userType: "teacher" | "student" })
+      ?.userType || "teacher";
 
   if (isStudentRoute(req)) {
     if (userRole !== "student") {
